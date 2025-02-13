ad2f3c9a0431856766d944aea555d2640d7f7b8c
LucasBParada <lucasparada2006@gmail.com>
2025-02-13 18:47:58 -0300

    Variáveis e funções no JS

---
+++ aula-0.1-variaveis-e-funcoes/js-aula-0.1.js
@@ -0,0 +1,23 @@
+//comentário JS
+/* comentátio
+em 
+bloco */
+
+//CTRL + ;
+
+//Declaração de variáveis no JS
+//CONST x LET x VAR
+//Evitar uso de VAR
+//No escopo globar cont não pode ser alterada
+
+const nome = "Lucas" //Precisa de um valor inicial
+let Nome //Permite iniciar a variável sem valor
+var cor = "Azul"
+let cidade = "Registro"
+cidade = "Jacupiranga"
+
+//Let pode alterar o valor
+
+const message = "Hello, world! Iniciando estudos com JavaScript"
+console.log(message)
+
